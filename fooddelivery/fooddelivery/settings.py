41305f7fae90b4654f152c894578615596dd1a4f
Aditya Godambe <godambeaditya@gmail.com>
2024-09-04 10:50:06 +0530

    refactor: Database Config

--- fooddelivery/fooddelivery/settings.py
+++ fooddelivery/fooddelivery/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'fooddelivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'zomato',
-#         'HOST': 'localhost',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'PORT': '3306',
-        
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'HOST': 'meticulous-empathy.railway.internal',
-        'USER': 'postgres',
-        'PASSWORD': 'XbffbMcJfrzrdyNxeIWLBlQCOFyYDeun',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zomato',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
+        
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'HOST': 'meticulous-empathy.railway.internal',
+#         'USER': 'postgres',
+#         'PASSWORD': 'XbffbMcJfrzrdyNxeIWLBlQCOFyYDeun',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
